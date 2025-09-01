deea3791cd71929be9d578bde67df34ccb34f469
iskrasavtsev <krasavcevivan@gamil.com>
2025-09-01 10:47:13 +0200

    Your commit message

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,10 @@
+import iskrasavtsev
+import Phoebe
+import Yinan
+import Yifan
+
+def team_intro():
+    print("This is Team YourTeamNameHere. We are:")
+    print(iskrasavtsev.get_my_name())
+    print(Phoebe.get_my_name())
+    print(Yinan.get_my_name())
