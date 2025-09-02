0d1d789985e9e006fce74397dfcabab5a3e5fa18
“olessyuh” <chernyakolesyaa@gmal.com>
2025-09-02 11:03:26 +0200

    done

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,1 +1,18 @@
 #3
+def solution_station_3(n: int) -> bool:
+    """
+    Returns True if the input integer is divisible by 3, otherwise False.
+    
+    Examples:
+    >>> solution_station_3(9)
+    True
+    >>> solution_station_3(84)
+    True
+    >>> solution_station_3(21455)
+    False
+    >>> solution_station_3(47840)
+    False
+    >>> solution_station_3(94632)
+    True
+    """
+    return n % 3 == 0
