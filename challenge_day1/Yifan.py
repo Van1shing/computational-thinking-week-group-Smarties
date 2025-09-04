0b2181f8f863961079658931ff7754b364f5dd85
Yifan Bai <baiyifan@re-byods-145-109-56-215.wireless.uva.nl>
2025-09-04 13:25:04 +0200

    yifan's_story

--- challenge_day1/Yifan.py
+++ challenge_day1/Yifan.py
@@ -1,3 +1,9 @@
 def get_my_name():
     return "Yifan Bai"
 print(get_my_name())
+
+paragraphs = [
+    "{name3} enjoys coding and analyzing data with {name1}, {name2}, {name4}, and {name5} at the university in Amsterdam.",
+    "In group projects, {name3} contributes clever ideas that help {name1}, {name2}, {name4}, and {name5} complete tasks efficiently while laughing together.",
+    "Outside of class, {name3} spends time with {name1}, {name2}, {name4}, and {name5}, sharing jokes and fun moments during study breaks."
+]
