2203aeba112d5a6276b9b7e3df440ca96e755907
Yinan <yinan.c1224@gmail.com>
2025-09-04 14:36:24 +0200

    aaa

--- challenge_day1/olessyuh.py
+++ challenge_day1/olessyuh.py
@@ -7,7 +7,7 @@ my_name = "Olesya"
 paragraphs = [
     f"{my_name} is focused on learning social science patterns together with {{name1}}, {{name2}}, {{name3}}, and {{name4}} in Amsterdam.",
     f"During group work, {my_name} supports {{name1}}, {{name2}}, {{name3}}, and {{name4}}, offering smart solutions and keeping the atmosphere cheerful.",
-    f"Outside of class, {my_name} spends time joking and chatting with {{name1}}, {{name2}}, {{name3}}, and {{name4}}, showing that studying can be fun and social."
+    f"Outside of class, {my_name} always spends time joking and chatting with {{name1}}, {{name2}}, {{name3}}, and {{name4}}, showing that studying can be fun and social."
 ]
 
 def my_story_name():
