eacc752e4c6cf245e5d285f8e9f678cc80959dca
Yinan <yinan.c1224@gmail.com>
2025-09-04 14:33:10 +0200

    1

--- challenge_day1/iskrasavtsev.py
+++ challenge_day1/iskrasavtsev.py
@@ -5,8 +5,8 @@ print(get_my_name())
 my_name = "Is"  # only define your own name
 
 paragraphs = [
-    f"{my_name} studies Computational Social Science in Amsterdam with {{name2}}, {{name3}}, {{name4}}, and {{name5}}. Together they explore patterns in society and learn new things every day.",
-    f"During group projects, {my_name} finds clever ways to solve problems while making {{name2}}, {{name3}}, {{name4}}, and {{name5}} laugh and stay motivated.",
+    f"{my_name} studies Computational Social Science at UvA with {{name2}}, {{name3}}, {{name4}}, and {{name5}}. Together they explore patterns in society and learn new things every day.",
+    f"During group projects, {my_name} always finds clever ways to solve problems while making {{name2}}, {{name3}}, {{name4}}, and {{name5}} laugh and stay motivated.",
     f"Even outside of class, {my_name} enjoys joking with {{name2}}, {{name3}}, {{name4}}, and {{name5}}, showing that learning can be both smart and fun."
 ]
 
