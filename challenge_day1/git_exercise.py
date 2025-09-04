a9fe83bf9709e1daee3a40d99e24149ef89a193e
iskrasavtsev <krasavcevivan@gamil.com>
2025-09-04 14:02:11 +0200

    sdf

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,31 +1,33 @@
-# Import team modules
 import iskrasavtsev
-import Phoebe
 import Yinan
 import Yifan
+import Phoebe
 import olessyuh
 
-# Import story paragraphs
-from iskrasavtsev import paragraphs as is_paragraphs
-from Yinan import paragraphs as yinen_paragraphs
-from Yifan import paragraphs as yifan_paragraphs
-from Phoebe import paragraphs as phoebe_paragraphs
-from olessyuh import paragraphs as olesya_paragraphs
+from is_story import paragraphs as is_paragraphs
+from yinen_story import paragraphs as yinen_paragraphs
+from yifan_story import paragraphs as yifan_paragraphs
+from phoebe_story import paragraphs as phoebe_paragraphs
+from olesya_story import paragraphs as olesya_paragraphs
 
-# Function to print team intro
+# Map variables from each file
+name1 = iskrasavtsev.my_name
+name2 = Yinan.my_name
+name3 = Yifan.my_name
+name4 = Phoebe.my_name
+name5 = olessyuh.my_name
+
 def team_intro():
     print("This is Team Smarties. We are:")
-    print(iskrasavtsev.get_my_name())
-    print(Phoebe.get_my_name())
-    print(Yinan.get_my_name())
-    print(Yifan.get_my_name())
-    print(olessyuh.get_my_name())
-    print()  # blank line after intro
+    print(iskrasavtsev.my_story_name())
+    print(Yinan.my_story_name())
+    print(Yifan.my_story_name())
+    print(Phoebe.my_story_name())
+    print(olessyuh.my_story_name())
+    print()
 
-# Function to print the story
 def print_story():
-    # Organize paragraphs per act
-    acts = [0, 1, 2]  # indices for first, second, third paragraph of each person
+    acts = [0, 1, 2]
     team_names = [
         ("Is", is_paragraphs),
         ("Yinen", yinen_paragraphs),
@@ -35,19 +37,13 @@ def print_story():
     ]
 
     for act in acts:
-        print(f"--- Act {act + 1} ---\n")
+        print(f"--- Act {act+1} ---\n")
         for author_name, paragraphs in team_names:
-            # Use f-string formatting in case paragraphs contain variables like {name1}, {name2}
             print(f"{author_name} says:")
             print(paragraphs[act].format(
-                name1="Is",
-                name2="Yinen",
-                name3="Yifan",
-                name4="Phoebe",
-                name5="Olesya"
+                name1=name1, name2=name2, name3=name3, name4=name4, name5=name5
             ) + "\n")
 
-# Run everything
 if __name__ == "__main__":
     team_intro()
     print_story()
