2a74114082dfe8a5a80f4080196b0afbd42e8862
“olessyuh” <chernyakolesyaa@gmal.com>
2025-09-04 14:27:52 +0200

    asd

--- challenge_day1/Phoebe.py
+++ challenge_day1/Phoebe.py
@@ -6,7 +6,7 @@ my_name = "Phoebe"
 
 paragraphs = [
     f"{my_name} studies Computational Social Science with {{name1}}, {{name2}}, {{name3}}, and {{name5}}, always curious about how society works.",
-    f"During projects, {my_name} explains ideas to {{name1}}, {{name2}}, {{name3}}, and {{name5}} using humor, helping the team understand difficult concepts.",
+    f"During projects, {my_name} explains new and clear ideas to {{name1}}, {{name2}}, {{name3}}, and {{name5}} using humor, helping the team understand difficult concepts.",
     f"With friends, {my_name} enjoys laughing and sharing stories with {{name1}}, {{name2}}, {{name3}}, and {{name5}}, making study sessions enjoyable."
 ]
 
