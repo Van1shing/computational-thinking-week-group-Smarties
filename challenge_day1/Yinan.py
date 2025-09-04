57e225cca36dd01b968274ac5dc6b11796f5a2be
Yinan <yinan.c1224@gmail.com>
2025-09-04 13:31:15 +0200

    yinan

--- challenge_day1/Yinan.py
+++ challenge_day1/Yinan.py
@@ -1,3 +1,8 @@
 def get_my_name():
     return "Yinan Chen"
 print(get_my_name())
+
+paragraphs = [
+    "{name2} loves discovering patterns in social behavior alongside {name1}, {name3}, {name4}, and {name5} in Amsterdam.",
+    "When working on assignments, {name2} helps {name1}, {name3}, {name4}, and {name5} stay focused, while also keeping the mood light and fun.",
+    "In free time, {name2} chats with {name1}, {name3}, {name4}, and {name5}, sharing funny observations about their studies and projects."]
