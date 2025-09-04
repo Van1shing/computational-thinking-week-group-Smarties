e10de373c32b4a93a486cb66531d65426704b26c
“olessyuh” <chernyakolesyaa@gmal.com>
2025-09-04 14:30:25 +0200

    imporved

--- challenge_day1/Yifan.py
+++ challenge_day1/Yifan.py
@@ -7,7 +7,7 @@ my_name = "Yifan"
 paragraphs = [
     f"{my_name} enjoys coding and analyzing data with {{name1}}, {{name2}}, {{name4}}, and {{name5}} at the university in Amsterdam.",
     f"In group projects, {my_name} contributes clever ideas that help {{name1}}, {{name2}}, {{name4}}, and {{name5}} complete tasks efficiently while laughing together.",
-    f"Outside of class, {my_name} spends time with {{name1}}, {{name2}}, {{name4}}, and {{name5}}, sharing jokes and fun moments during study breaks."
+    f"Outside of class, {my_name} spends a lot of time with {{name1}}, {{name2}}, {{name4}}, and {{name5}}, sharing jokes and fun moments during study breaks."
 ]
 
 def my_story_name():
